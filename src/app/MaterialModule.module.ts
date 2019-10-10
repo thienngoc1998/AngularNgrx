de9e7fcff610b63928a5023dcd491195ebe4b639
thienngoc98 <suppermabu@gmail.com>
2019-10-10 22:16:34 +0700

    save code

---
+++ src/app/MaterialModule.module.ts
@@ -0,0 +1,9 @@
+
+import {MatButtonModule} from '@angular/material/button';
+import  {NgModule} from "@angular/core";
+
+@NgModule({
+        imports: [MatButtonModule],
+        exports: [MatButtonModule]
+})
+export class MaterialModule { }
